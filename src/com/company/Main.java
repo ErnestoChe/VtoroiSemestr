83ce10e3ce3b9d9478e7e364d58de229ff22c341
Ernest <ernestfilippov@mail.ru>
2019-02-15 12:05:22 +0300

    com

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,9 +7,9 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        fileReader a = new fileReader();
-        String[] l = a.main();
-        int len = a.count();
+        fileReader file = new fileReader();
+        //String[] l = a.main();
+        /*int len = a.getLength();
         int[] date = new int[len];
         double[] open = new double[len];
         double[] close = new double[len];
@@ -22,14 +22,56 @@ public class Main {
             high[i] = Double.parseDouble(l[i].substring(35,44));
             low[i] = Double.parseDouble(l[i].substring(45,54));
             System.out.println(i + "\t" + date[i] + "\t" + open[i] + "\t" + close[i] + "\t" + high[i] + "\t" + low[i]);
+        }*/
+        double[] close = file.getClose();
+        int len = close.length;
+        int l = len - 1;
+        double t = 0;           //t
+        double t2 = 0;       //t^2
+        double y = 0;       //y
+        double ty = 0;     //ty
+        double a, b;
+        /*for(int i = 1; i<len; i++){
+            System.out.println(i + "\t" + close[i]);
+            t += i; //t
+            t2 += Math.pow(i, 2);    //t^2
+            y += close[i];  //y
+            ty += close[i] * i; //ty
+        }
+        t /= len;   //t
+        t2 /= len;   //t^2
+        y /= len;   //y
+        ty /= len; //ty
+
+        b = (ty - t * y)/(t2 - t * t);
+        a = y - b * t;*/
+
+        y = close[1] + close[2] + close[3];
+        t = (1 + 2 + 3) / 3;
+        ty = close[1] + 2 * close[2] + 3 * close[3];
+        t2 = (1 + 2^2 + 3^2) / 3;
+
+        b = (ty - t * y) / ()
+
+
+        /*b = (ty * t - y) / (Math.pow(t, 2) - l);
+        a = (y - b * l) / t;*/
+
+        System.out.println(a);
+        System.out.println(b);
+
+        double[] pred = new double[len];
+        for(int i = 1; i<len; i++){
+            pred[i] = a + b * (i);
+            System.out.println(i+ "\t" + close[i] + "\t" + pred[i]);
         }
         try(FileWriter writer = new FileWriter("C:\\Users\\pc\\Desktop\\1 КУРС\\ICT\\zapis.txt", false))
         {
-            for(int i = 1; i< open.length; i++){
+            for(int i = 1; i< close.length; i++){
 
-                writer.write(String.valueOf(open[i]));
+                writer.write(String.valueOf(close[i]));
                 writer.append('\t');
-                writer.append(Double.toString(close[i]));
+                //writer.append(Double.toString(close[i]));
                 writer.append('\n');
             }
 
--- src/com/company/fileReader.java
+++ src/com/company/fileReader.java
@@ -4,9 +4,9 @@ import java.io.*;
 
 public class fileReader {
     String[] lines = new String[40];
-    int i = 0;
 
-    public String[] main() {
+    public int main() {
+        int i = 0;
         try {
             File file = new File("C:\\Users\\pc\\Desktop\\1 КУРС\\ICT\\EURUSD_190101_190201.txt");
             //создаем объект FileReader для объекта File
@@ -28,9 +28,16 @@ public class fileReader {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return lines;
-    }
-    public int count(){
         return i;
+        //return lines;
     }
+    public double[] getClose(){
+        int j = main();
+        double[] close = new double[j];
+        for(int k = 1; k<j; k++){
+            close[k] = Double.parseDouble(lines[k].substring(55,64));
+        }
+        return close;
+    }
+
 }
