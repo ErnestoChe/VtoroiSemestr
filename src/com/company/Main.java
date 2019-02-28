d5f0be47664426251cbb8e2f0b8c73f6da7cc8c9
Ernest <ernestfilippov@mail.ru>
2019-02-28 22:46:56 +0300

    1)stroim matrcici 2)reshaem 3)?????? 4)PROFIT!!!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,72 +8,67 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         fileReader file = new fileReader();
-        //String[] l = a.main();
-        /*int len = a.getLength();
-        int[] date = new int[len];
-        double[] open = new double[len];
-        double[] close = new double[len];
-        double[] high = new double[len];
-        double[] low = new double[len];
-        for(int i = 1; i<34; i++){
-            date[i] = Integer.parseInt(l[i].substring(9,17));
-            open[i] = Double.parseDouble(l[i].substring(25,34));
-            close[i] = Double.parseDouble(l[i].substring(55,64));
-            high[i] = Double.parseDouble(l[i].substring(35,44));
-            low[i] = Double.parseDouble(l[i].substring(45,54));
-            System.out.println(i + "\t" + date[i] + "\t" + open[i] + "\t" + close[i] + "\t" + high[i] + "\t" + low[i]);
-        }*/
         double[] close = file.getClose();
+        double[] open = file.getOpen();
+        double[] high = file.getHigh();
+        double[] low = file.getLow();
+        zapis(close);
+
+        Matrix m = new Matrix(factori(file));
+        Matrix m1 = new Matrix(closi(file), 1);
+        Matrix m_t = m.transpose();
+
+        double[] otveti = test(factori(file), closi(file));
+        for(int i = 0; i<10; i++){
+
+            double c = otveti[0] + otveti[1] * open[i] + otveti[2] * high[i] + otveti[3] * low[i];
+            System.out.println("real close " + close[i]);   //реальная цена
+            System.out.format("%.4f%n", c);         //предполагаемая цена
+            System.out.println(Math.abs(close[i] - c));     //сравнение исходных и полученных данных
+        }
+    }
+
+    public static void zapis(double[] close){
+        try(FileWriter writer = new FileWriter("C:\\Users\\pc\\Desktop\\1 КУРС\\ICT\\zapis.txt", false))
+        {
+            for(int i = 1; i< close.length; i++){
+                writer.write(String.valueOf(close[i]));
+                writer.append('\t');
+                writer.append('\n');
+            }
+            writer.flush();
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public double[] mnk(fileReader file){
         int len = file.main();
         double t = 0;           //t
         double t2 = 0;       //t^2
         double y = 0;       //y
         double ty = 0;     //ty
         double a, b;
+        double[] close = file.getClose();
         for(int i = 1; i<len; i++){
             t += i/len;
             y += close[i]/len;
             ty += i * close[i]/len;
             t2 += Math.pow(i,2)/len;
         }
-
         b = (ty - t * y)/(t2 - t*t);
         a = y - b * t;
-
         System.out.println(a);
         System.out.println(b);
-
-
         double[] pred = new double[len];
         for(int i = 1; i<len; i++){
             pred[i] = a + b * i;
-            //System.out.println(i+ "\t" + (close[i]) + "\t" + pred[i]);
         }
-        try(FileWriter writer = new FileWriter("C:\\Users\\pc\\Desktop\\1 КУРС\\ICT\\zapis.txt", false))
-        {
-            for(int i = 1; i< close.length; i++){
+        return pred;
 
-                writer.write(String.valueOf(close[i]));
-                writer.append('\t');
-                writer.append('\n');
-            }
-
-            writer.flush();
-        }
-        catch(IOException ex){
-            System.out.println(ex.getMessage());
-        }
-        //test(factori(file), closi(file));
-        Matrix m = new Matrix(factori(file));
-        System.out.println(m.getColumnDimension());
-        System.out.println(m.getRowDimension());
-
-        Matrix m1 = new Matrix(closi(file), 1);
-        System.out.println(m1.getColumnDimension());
-        System.out.println(m1.getRowDimension());
-        m1.print(5, 5);
-        m.print(5,5);
     }
+
     public static double[][] factori(fileReader file){
         int k = file.main();
         double[] open = file.getOpen();
@@ -98,20 +93,33 @@ public class Main {
         }
         return y;
     }
-    public static void test(double[][]ff, double[]yy){
-        Matrix A1=new Matrix(ff);
-        double[] z = new double[5];
-        //A1.print(10, 2);
-        Matrix B1=A1.transpose();
-        Matrix F1=A1.times(B1);
-        Matrix F4=F1.inverse();
-        Matrix F2=F4.times(A1);
-        Matrix C=new Matrix(yy,4);
-        Matrix F3=F2.times(C);
-        z=F3.getColumnPackedCopy();
-        System.out.println("Коэффициенты:");
-        for (int i = 0; i < 4; i++)
-            System.out.println("B"+i+"="+z[i]);
+    public static double[] test(double[][]ff, double[]yy){
+        Matrix X = new Matrix(ff);
+        log("X", X);
+        Matrix Xt = X.transpose();
+        log("Xt", Xt);
+        Matrix X1 = X.times(Xt);
+        log("x1", X1);
+        Matrix Xinv = X1.inverse();
+        log("Xinv", Xinv);
+        Matrix Y = new Matrix(yy, 1);
+        log("y", Y);
+        Matrix Xt2 = Xt.times(Xinv);
+        log("xt2", Xt2);
+        Matrix B = Y.times(Xt2);
+        log("B", B);
 
+        double[] z = new double[4];
+        z = B.getColumnPackedCopy();
+        System.out.println("Коэффициенты:");
+        for (int i = 0; i < 4; i++){
+            System.out.println("B"+i+"="+z[i]);
+        }
+        return z;
+    }
+    public static void log(String name, Matrix M){
+        System.out.println(name);
+        System.out.println(M.getRowDimension() + " x " + M.getColumnDimension());
+        System.out.println();
     }
 }
--- src/com/company/fileReader.java
+++ src/com/company/fileReader.java
@@ -3,13 +3,14 @@ package com.company;
 import java.io.*;
 
 public class fileReader {
-    String[] lines = new String[370];
+    String[] lines = new String[1500];
 
     public int main() {
         int i = 0;
         try {
             //File file = new File("C:\\Users\\pc\\Desktop\\1 КУРС\\ICT\\EURUSD_190101_190201.txt");
-            File file = new File("C:\\Users\\pc\\Desktop\\1 КУРС\\ICT\\EURUSD_180216_190216.txt");
+            //File file = new File("C:\\Users\\pc\\Desktop\\1 КУРС\\ICT\\EURUSD_180216_190216.txt");
+            File file = new File("C:\\Users\\pc\\Desktop\\1 КУРС\\ICT\\EURUSD_150216_190216.txt");
             //создаем объект FileReader для объекта File
             FileReader fr = new FileReader(file);
             //создаем BufferedReader с существующего FileReader для построчного считывания
@@ -64,5 +65,4 @@ public class fileReader {
         }
         return low;
     }
-
 }
